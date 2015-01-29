dd04ccc4d8a1539fe7b924de5f493ccea7d95259
litopia <litopia.li@gmail.com>
2015-01-29 15:15:31 -0800

    basic functionalities done

--- script.js
+++ script.js
@@ -18,15 +18,9 @@ window.onload = function() {
 		if (video.paused == true) {
 			// Play the video
 			video.play();
-
-			// Update the button text to 'Pause'
-			playButton.innerHTML = "Pause";
 		} else {
 			// Pause the video
 			video.pause();
-
-			// Update the button text to 'Play'
-			playButton.innerHTML = "Play";
 		}
 	});
 
@@ -80,12 +74,14 @@ window.onload = function() {
 		seekBar.value = value;
 	});
 
+
 	// Pause the video when the seek handle is being dragged
 	seekBar.addEventListener("mousedown", function() {
 		video.pause();
 	});
 
 	// Play the video when the seek handle is dropped
+	// TODO: track the current step on handler release
 	seekBar.addEventListener("mouseup", function() {
 		video.play();
 	});
@@ -94,12 +90,14 @@ window.onload = function() {
 	volumeBar.addEventListener("change", function() {
 		// Update the video volume
 		video.volume = volumeBar.value;
-	});
+	})
 
 
 
 	var setupVideoPlayer = function(){
-		var steps = document.getElementById('video-controls').getAttribute('data-stops').split(' ');
+		var steps = document.getElementById('video-controls').getAttribute('data-stops').split(' '),
+				currentStep = 1,
+				isSectionEnded = false;
 
 		//Append step indicator to the video player's time track
 		appendSteps(steps);
@@ -107,24 +105,78 @@ window.onload = function() {
 		//Step indicator onclick, set video's currentTime to the step time
 		$('.step-indicator').on('click', function(){
 			var that = this;
-			video.currentTime = that.getAttribute('data-time');
+			video.currentTime = that.getAttribute('data-time')
+			currentStep = that.textContent - 1;
+			isSectionEnded = true;
 		})
 
 		//Stop when it comes to next step
+		video.addEventListener('timeupdate', function(){
+			for (var i = 1; i < steps.length; i++) {
+				if(Math.floor(video.currentTime) == steps[i] && currentStep == i){
+					video.pause();
+					$(playButton).text('Next Step');
+					isSectionEnded = true;
+				}
+			};
+		})
 
-		//Toggle accordion with the according step
+		//Play to see next step
+		video.addEventListener('play', function(){
+			if(isSectionEnded){
+				currentStep ++;
+				toggleAccordion(currentStep);
+				isSectionEnded = false;
+			}
+		})
+
+		// Show play button on video pause
+		video.addEventListener('pause', function(){
+			$(playButton).removeClass('hide');
+			if(isSectionEnded){
+				playButton.innerHTML = "Next Step"
+			}else{
+				playButton.innerHTML = 'Play';
+			}
+		})
+
+		// Hide play button on video playing
+		video.addEventListener('playing', function(){
+			$(playButton).addClass('hide');
+			playButton.innerHTML = 'Pause';
+		})
+		//Select accordion to according point of the video
+		selectAccordion(steps)
 	}
 
 	var appendSteps = function(array){
 		for (var i = 0; i < array.length; i++) {
 			var stepTime = array[i],
-					stepPosition = $(seekBar).width() * stepTime/video.duration,
-					$tagSpan = $('<span class="step-indicator"/>').css('left', stepPosition).text(i + 1).attr('data-time',stepTime);
+					stepPosition = stepTime/video.duration * 100,
+					$tagSpan = $('<span class="step-indicator"/>').css('left', stepPosition + '%').text(i + 1).attr('data-time',stepTime);
 			$('.seekbar-container').append($tagSpan);
 		};
 	}
 
+	//Toggle accordion with the according step
+	var toggleAccordion = function(step){
+		var accordion = $('.collapse')[step -1];
+		$('.collapse').not(accordion).removeClass('in');
+		if(!$(accordion).hasClass('in')){
+			$(accordion).addClass('in');
+		}
+	}
+
+	//Select steps to go to the video point
+	var selectAccordion = function(steps){
+		$('.instruction').on('click', function(e){
+			var target = e.target.text.trim().split(' ')[1] - 1;
+			$('.step-indicator')[target].click();
+		})
+	}
+
 	setupVideoPlayer();
 }
+
 
 
